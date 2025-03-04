57ed5edbf190c2173aae2a5568a4f97165f479e6
Prodan Cosmin Daniel <pro.cosmin2004@gmail.com>
2025-03-04 20:06:46 +0200

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   
   const [oras,setOras] = useState("Brasov");
   const [data,setData] = useState(null);
+  const [data5zile,setData5zile] = useState(null);
   const [iconIndex,setIconIndex] = useState(0);
+  const [iconIndex5,setIconIndex5] = useState([0,1,2,0,1]);
   const icons = ["/clouds.svg","/rain.svg","/snow.svg","/sun.svg"];
   
 
@@ -18,7 +20,11 @@ function App() {
         `https://api.openweathermap.org/data/2.5/weather?q=${oras}&appid=${apiKey}&units=metric`
       );
       setData(response.data);
-      console.log(response.data);
+      
+
+      const response2 = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${oras}&appid=${apiKey}&units=metric`);
+      setData5zile(response2.data);
+      console.log(response2.data);
       
     
     } catch (error) {
@@ -35,6 +41,7 @@ function App() {
   useEffect(() => {
    if(data){
     handleIcon();
+    handleIcon5();
    }
   }, [data]);
 
@@ -65,18 +72,34 @@ function App() {
       setIconIndex(3);
     else if(weather === 'clouds')
       setIconIndex(0);
-
-    
  }
+
+ function handleIcon5() {
+  if (!data5zile) return;
+
+  const indices = [6, 14, 21, 29, 37]; 
+  const newIcons = indices.map(i => {
+    const weather = data5zile.list[i]?.weather[0]?.main.toLowerCase();
+    switch(weather) {
+      case 'snow': return 2;
+      case 'rain': return 1;
+      case 'clear': return 3;
+      case 'clouds': return 0;
+      default: return 0;
+    }
+  });
+  
+  setIconIndex5(newIcons);
+}
 
   return (
    <>
-   {data ? (
+   {data && data5zile ? (
     <>
-   <div className="background-image ">
-     <div className=' lg:ml-50 lg:mt-25  lg:mr-45 fixed inset-0 bg-gradient-to-r from-black/55 to-black/15 lg:rounded-t-xl '>
+   <div className="background-image">
+     <div className=' lg:ml-50 lg:mt-25  lg:mr-45 fixed inset-0 bg-gradient-to-r from-black/55 to-black/15 overflow-y-auto  lg:overflow-y-hidden pb-0 mb-0'>
       
-      <div className='flex justify-center lg:pt-5 lg:pb-5'>
+      <div className='flex justify-center lg:pt-5 lg:pb-4'>
       <input className=' border-1 p-2 outline-none rounded-l-[25px] border-r-0 w-[75%] mt-5 text-2xl' 
       placeholder='Search your location'
       value={oras}
@@ -102,7 +125,7 @@ function App() {
   </div>
 
   
-  <div className='grid grid-cols-3 grid-rows-2 gap-4 place-self-center lg:col-span-2 gap-20'>
+  <div className='grid grid-cols-3 grid-rows-2 gap-4 place-self-center lg:col-span-2 gap-20 py-0 mt-0 '>
   <div>
    <p className=''>{Math.round(data.main.temp_max)}°C</p>
    <p className='text-sm'>HIGH</p>
@@ -128,6 +151,44 @@ function App() {
    <p className='text-sm'>sunset</p>
   </div>
   </div>
+
+  <div className='lg:col-span-4 overflow-x-auto'>
+  <div className='grid grid-cols-5 text-center gap-20 min-w-[850px]  lg:text-2xl p-4 3xl:text-3xl 3xl:p-5 lg:mt-16 mt-4 '>
+   
+    <div>
+      <span>{moment.unix(data5zile.list[6].dt).format("dddd")}</span>
+      <p className='mt-2 3xl:mt-5'>Temperature {Math.round(data5zile.list[6].main.feels_like)}°C</p>
+      <img className=' mt-3 w-[25%]  3xl:mt-6 place-self-center' src={icons[iconIndex5[0]]}></img>
+    </div>
+
+    <div>
+    <span>{moment.unix(data5zile.list[14].dt).format("dddd")}</span>
+    <p className='mt-2 3xl:mt-5'>Temperature {Math.round(data5zile.list[14].main.feels_like)}°C</p>
+    <img className=' mt-3 w-[25%]  3xl:mt-6 place-self-center' src={icons[iconIndex5[1]]}></img>
+    </div>
+
+    <div>
+    <span >{moment.unix(data5zile.list[21].dt).format("dddd")}</span>
+    <p className='mt-2 3xl:mt-5' >Temperature {Math.round(data5zile.list[21].main.feels_like)}°C</p>
+    <img className=' mt-3 w-[25%]  3xl:mt-6 place-self-center' src={icons[iconIndex5[2]]}></img>
+    </div>
+
+    <div>
+    <span>{moment.unix(data5zile.list[29].dt).format("dddd")}</span>
+    <p className='mt-2 3xl:mt-5'>Temperature {Math.round(data5zile.list[29].main.feels_like)}°C</p>
+    <img className=' mt-3 w-[25%]  3xl:mt-6 place-self-center' src={icons[iconIndex5[3]]}></img>
+    </div>
+    <div>
+
+    <span>{moment.unix(data5zile.list[37].dt).format("dddd")}</span>
+    <p className='mt-2 3xl:mt-5'>Temperature {Math.round(data5zile.list[37].main.feels_like)}°C</p>
+    <img className=' mt-3 w-[25%]  3xl:mt-6 place-self-center' src={icons[iconIndex5[4]]}></img>
+    </div>
+
+
+  </div>
+  </div>
+
 </div>
      </div>
      </div>
